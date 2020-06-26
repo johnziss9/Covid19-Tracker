0316ef30fb84dbb873cee902892732ef60e89dd7
John Zissimou <ZissimouJ@b2b.regn.net>
2020-06-26 15:13:39 +0100

    Added data in state in App.js and passed them to Cards component so they can be accessed. Also installed MaterialUI.

--- src/App.js
+++ src/App.js
@@ -2,21 +2,27 @@ import React from 'react';
 
 import { Cards, Chart, CountryPicker } from './components';
 import styles from './App.module.css';
-
 import { fetchData } from './api';
 
 class App extends React.Component {
 
-    async componentDidMount() {
-        const data = await fetchData();
+    state = {
+        data: {},
+    }
 
-        console.log(data);
+    async componentDidMount() {
+        const fetchedData = await fetchData();
+
+        this.setState({ data: fetchedData });
     }
 
     render() {
+
+        const { data } = this.state;
+
         return (
             <div className={styles.container}>
-                <Cards />
+                <Cards data={data} />
                 <CountryPicker />
                 <Chart />
             </div>
