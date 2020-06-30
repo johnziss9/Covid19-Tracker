9807c828fdc2e632ac37b4bfb778a9de6eba593b
John Zissimou <ZissimouJ@b2b.regn.net>
2020-06-30 11:45:52 +0100

    Modified fetchData in API index.js to get the country data if country is selected. Got the fetchData in App.js so they can be passed to the other components. Passed the data to Chart. Added the bar chart and got it to show the data when a country is selected. Added header image and applied it to App.js. Added some styling for mobile.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { Cards, Chart, CountryPicker } from './components';
 import styles from './App.module.css';
 import { fetchData } from './api';
 
+import covid19Image from './images/image.png';
+
 class App extends React.Component {
 
     state = {
@@ -18,20 +20,25 @@ class App extends React.Component {
     }
 
     handleCountryChange = async (country) => {
-        console.log(country);
+
+        const fetchedData = await fetchData(country);
+
+        this.setState({ data: fetchedData, country: country });
+
         // fetch the data
         // set the state
     }
 
     render() {
 
-        const { data } = this.state;
+        const { data, country } = this.state;
 
         return (
             <div className={styles.container}>
+                <img className={styles.image} src={covid19Image} alt="COVID-19" />
                 <Cards data={data} />
                 <CountryPicker handleCountryChange={this.handleCountryChange} />
-                <Chart />
+                <Chart data={data} country={country} />
             </div>
         )
     }
