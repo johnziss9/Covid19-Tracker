388525ea6552c187e414493f16fefe244a1999d7
John Zissimou <ZissimouJ@b2b.regn.net>
2020-07-03 15:57:00 +0100

    Added the navbar (links not working currently) and styled it.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import { fetchData } from './api';
 
 import covid19Image from './images/image.png';
 
+import cx from 'classnames';
+
 class App extends React.Component {
 
     state = {
@@ -33,8 +35,21 @@ class App extends React.Component {
         return (
             <div className={styles.container}>
                 <img className={styles.image} src={covid19Image} alt="COVID-19" />
-                <Cards data={data} /> 
+                
+                <nav className={cx(styles.navbar, "navbar navbar-expand-lg justify-content-center")}>
+                    <div>
+                        <ul className={cx(styles.navbarNav, "navbar-nav")}>
+                            <li className={cx(styles.navbarItem, "nav-item")} onClick={this.handleCardsLink}>
+                                <a className={cx(styles.navbarLink, "nav-link")} href="/">Cards</a>
+                            </li>
+                            <li className={cx(styles.navbarItem, "nav-item")} onClick={this.handleChartLink}>
+                                <a className={cx(styles.navbarLink, "nav-link")} href="/">Chart</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
                 <CountryPicker handleCountryChange={this.handleCountryChange} />
+                <Cards data={data} /> 
                 <Chart data={data} country={country} />
             </div>
         )
